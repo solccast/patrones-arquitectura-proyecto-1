746ea103b44a6af7b5177e3ace7b5f1866ac320d
Franco Kumichel <francokumichel1996@gmail.com>
2025-04-14 11:54:17 -0300

    Agrego comando para crear db y agregar data

--- app-patrones/src/api/__init__.py
+++ app-patrones/src/api/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from src.api.config import config
+from src.core.database import db
+from src.core.database import seeds
 
 def create_app(env="development", static_folder="../../static"):
 
     app = Flask(__name__, static_folder=static_folder)
     app.config.from_object(config[env])
+
+    db.init_app(app)
+
+    @app.cli.command(name="resetdb")
+    def resetdb():
+        db.reset_db()
+        seeds.cargar_seeds()
 
     return app
