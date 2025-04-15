dc8c02af799c158452afc9163c9911edbc7109a1
Sol Angela Chilaca Castro <solaccastro@gmail.com>
2025-04-15 16:07:49 -0300

    modif del endpoint

--- app-patrones/src/api/routes/__init__.py
+++ app-patrones/src/api/routes/__init__.py
@@ -2,6 +2,13 @@ from flask import Blueprint
 from src.api.routes.persona import persona_bp
 from src.api.routes.direccion import direccion_bp
 
+bp_api = Blueprint("api", __name__, url_prefix="/api");
+
 def register_blueprints(app):
-    app.register_blueprint(persona_bp)
-    app.register_blueprint(direccion_bp)
+
+    """Registra los blueprints de la API """
+    bp_api.register_blueprint(persona_bp)
+    bp_api.register_blueprint(direccion_bp)
+
+
+    app.register_blueprint(bp_api)
